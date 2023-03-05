c0b3a27b64d89cfb520ccdddabcd1061ff465230
davejinyoung <davejinyoung@uvic.ca>
2023-03-05 00:07:52 -0800

    Created HTML skeleton for all pages and made routes for them. Created input fields for listing links

---
+++ website/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    return render_template("base.html", text="TESTING")
+
+
+@views.route('/fb')
+def face():
+    return render_template("face.html", text="TESTING")
+
+@views.route('/craig')
+def craig():
+    return render_template("craig.html", text="TESTING")
+
+@views.route('/about')
+def about():
+    return render_template("about.html", text="TESTING")
