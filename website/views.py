8c09ea7b9582f8655f49596b4c2236b1017b2a15
davejinyoung <davejinyoung@uvic.ca>
2023-03-19 18:28:31 -0700

    Added ability to handle post request. Adjusted the location of the submit button.

--- website/views.py
+++ website/views.py
@@ -1,23 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import re
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return render_template("base.html", text="TESTING")
+    return render_template("base.html")
 
 
 @views.route('/fb', methods=['GET', 'POST'])
 def face():
-    return render_template("face.html", text="TESTING")
+    if request.method == 'POST':
+        fb_link = request.form.get('link')
+        root = 'https://www.facebook.com/marketplace/item/'
+        if re.match(root, fb_link) is not None:
+            print("the link is correct")
+
+    return render_template("face.html")
 
 
 @views.route('/craig', methods=['GET', 'POST'])
 def craig():
-    return render_template("craig.html", text="TESTING")
+    if request.method == 'POST':
+        location = request.form.get('location')
+        cg_link = request.form.get('link')
+        root = f'https://{location}.craigslist.org/'
+        if re.match(root, cg_link) is not None:
+            print("the link is correct")
+
+    return render_template("craig.html")
 
 
 @views.route('/about')
 def about():
-    return render_template("about.html", text="TESTING")
+    return render_template("about.html")
